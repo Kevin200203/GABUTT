a99694ebe851133b1d8ed962ebce447728d207ca
kevin200203 <kevinsiringo756@gmail.com>
2021-11-29 21:39:09 +0700

    perbaikan nama

--- Tugas Besar Pemrograman.c
+++ Tugas Besar Pemrograman.c
@@ -2,9 +2,9 @@
 						Tugas Besar Pemrograman
 TANGGAL PEMBUATAN 11/23/2021
 OLEH :  -KEVIN SIRINGORINGO
-		-I Gede Ari Wisnu Sanjaya
+        -I Gede Ari Wisnu Sanjaya
 REVISI : 0
-DESKRIPSI : Membuat Game Windows Controller 
+DESKRIPSI : Membuat Game Windows Console Tic Tac Toe
 ******************************************************************************************************/
 
 #include <stdio>
